97292a04ad8c749d303b133894968b162790bc88
kevin untrojb <kuntrojb@gmail.com>
2021-09-01 19:20:42 -0300

    added lcs problem

---
+++ longestCommonSubsequence/lcs.go
@@ -0,0 +1,30 @@
+package longestCommonSubsequence
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	s1 := len(text1)
+	s2 := len(text2)
+	opt := make([][]int, s1+1)
+	for i := range opt {
+		opt[i] = make([]int, s2+1)
+	}
+
+	for i := 1; i <= s1; i++ {
+		for j := 1; j <= s2; j++ {
+			if text1[i-1] == text2[j-1] {
+				opt[i][j] = opt[i-1][j-1] + 1
+			} else {
+				opt[i][j] = max(opt[i-1][j], opt[i][j-1])
+			}
+
+		}
+	}
+
+	return opt[s1][s2]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
